7acf40f75339aa3e82f8e8555960046f73f07112
lekhobola <lekhobola@gmail.com>
2021-07-02 11:06:06 +0200

    Update CompanyController.java author

--- src/main/java/com/nightsbridge/demo/controller/CompanyController.java
+++ src/main/java/com/nightsbridge/demo/controller/CompanyController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 /**
  * The type Company controller.
  *
- * @author Givantha Kalansuriya
+ * @author Lekhobola Tsoeunyane
  */
 @RestController
 @RequestMapping("employees-demo/api/v1")
